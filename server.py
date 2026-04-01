4dabab72f0072a9a48bb91d5cc6e6728e71a4016
Shreeshaa <shreeshaa2007@gmail.com>
2026-04-01 22:38:18 +0530

    Fix import path issue

--- server.py
+++ server.py
@@ -1,28 +1,33 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 import sys
 import os
 
+# ✅ Force Python to find project folders
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from fastapi import FastAPI
 from env.environment import FishFarmEnv
 
 app = FastAPI()
+
 env = FishFarmEnv()
 
-class StepRequest(BaseModel):
-    actions: list
 
 @app.get("/")
 def home():
     return {"message": "Fish Farm API Running 🚀"}
+
 
 @app.post("/reset")
 def reset():
     state = env.reset()
     return {"state": state}
 
+
 @app.post("/step")
-def step(req: StepRequest):
-    state, reward, done, _ = env.step(req.actions)
-    return {"state": state, "reward": reward, "done": done}
+def step(actions: list):
+    state, reward, done, _ = env.step(actions)
+    return {
+        "state": state,
+        "reward": reward,
+        "done": done
+    }
